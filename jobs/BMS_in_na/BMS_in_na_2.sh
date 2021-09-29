d7782bd0b6d0126c59e3efc0d17d065dfaf3b848
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-09-29 20:47:56 +0330

    gres=gpu:v100:4

--- jobs/BMS_in_na/BMS_in_na_2.sh
+++ jobs/BMS_in_na/BMS_in_na_2.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name=BMS_in_na_2
 #SBATCH --output=%x-%j.out
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:v100:4
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem=127000M
 #SBATCH --time=3-00:00
