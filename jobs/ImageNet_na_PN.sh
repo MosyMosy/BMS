ef99c9f9cc6ddd9e2a859a13364d52b816fa28fc
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-10-18 10:50:21 +0330

    ImageNet_finetune_PN

--- jobs/ImageNet_na_PN.sh
+++ jobs/ImageNet_na_PN.sh
@@ -67,11 +67,11 @@ cd BMS
 
 python ImageNet_na_PN.py --dir ./logs/ImageNet_na_PN/ --arch resnet18 --data ./data/ILSVRC/Data/CLS-LOC --gpu 0
 
-python ImageNet_finetune.py --save_dir ./logs/ImageNet_na_PN/EuroSAT --target_dataset EuroSAT --subset_split datasets/split_seed_1/EuroSAT_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
-python ImageNet_finetune.py --save_dir ./logs/ImageNet_na_PN/CropDisease --target_dataset CropDisease --subset_split datasets/split_seed_1/CropDisease_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
-python ImageNet_finetune.py --save_dir ./logs/ImageNet_na_PN/ISIC --target_dataset ISIC --subset_split datasets/split_seed_1/ISIC_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
+python ImageNet_finetune_PN.py --save_dir ./logs/ImageNet_na_PN/EuroSAT --target_dataset EuroSAT --subset_split datasets/split_seed_1/EuroSAT_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
+python ImageNet_finetune_PN.py --save_dir ./logs/ImageNet_na_PN/CropDisease --target_dataset CropDisease --subset_split datasets/split_seed_1/CropDisease_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
+python ImageNet_finetune_PN.py --save_dir ./logs/ImageNet_na_PN/ISIC --target_dataset ISIC --subset_split datasets/split_seed_1/ISIC_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone &
 wait
-python ImageNet_finetune.py --save_dir ./logs/ImageNet_na_PN/ChestX --target_dataset ChestX --subset_split datasets/split_seed_1/ChestX_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone
+python ImageNet_finetune_PN.py --save_dir ./logs/ImageNet_na_PN/ChestX --target_dataset ChestX --subset_split datasets/split_seed_1/ChestX_labeled_80.csv --embedding_load_path ./logs/ImageNet_na_PN/checkpoint_best.pkl --freeze_backbone
 
 
 
