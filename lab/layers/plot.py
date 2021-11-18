403800ba5153591c20e51f0e3d336a3dba446a99
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-18 16:08:00 +0330

    New layer plot

--- lab/layers/plot.py
+++ lab/layers/plot.py
@@ -299,7 +299,7 @@ models.append(load_checkpoint2(
     ResNet10(), 'logs/STARTUP/EuroSAT/checkpoint_best.pkl', device))
 
 
-b_size = 2
+b_size = 256
 transform = EuroSAT_few_shot.TransformLoader(
     224).get_composed_transform(aug=True)
 transform_test = EuroSAT_few_shot.TransformLoader(
