879333b7d7c33c21fd14dc275de1c5401381623e
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2021-11-19 18:51:48 +0330

    Final

--- lab/layers_running_state/BNp.py
+++ lab/layers_running_state/BNp.py
@@ -20,8 +20,8 @@ class BatchNorm2d_plus(nn.BatchNorm2d):
     def forward(self, input):
         self._check_input_dim(input)
 
-        mean = input.mean([0, 2, 3])[None, :, None, None]
-        var = input.var([0, 2, 3], unbiased=False)[None, :, None, None]
+        mean = self.running_mean[None, :, None, None]
+        var = self.running_var[None, :, None, None]
         self.input = input        
         self.before_affine = (self.input.clone() - mean)/ torch.sqrt(var + self.eps)        
         self.output = self.before_affine.clone() * self.weight[None, :, None, None] + self.bias[None, :, None, None]
